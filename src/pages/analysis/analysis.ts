b3d8ac6e0bf5d5c49e7a766e8f8f1f4110654034
Robin Guerin <guerin28.robin@gmail.com>
2017-11-29 09:07:30 +0100

    init analysis part

--- src/pages/analysis/analysis.ts
+++ src/pages/analysis/analysis.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+  selector: 'page-analysis',
+  templateUrl: 'analysis.html'
 })
-export class AboutPage {
+export class AnalysisPage {
 
   constructor(public navCtrl: NavController) {
 
